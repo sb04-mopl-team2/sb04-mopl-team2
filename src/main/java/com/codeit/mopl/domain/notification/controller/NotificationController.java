f2e6a81e9a3fe23eac2da861329a25c19ef63d12
KwonYongJin <chaoskyj@gmail.com>
2025-11-12 13:32:55 +0900

    feat(notification) : 알림 컨트롤러, 서비스 파일 생성 및 레포지터리, 매퍼 작성

--- src/main/java/com/codeit/mopl/domain/notification/controller/NotificationController.java
+++ src/main/java/com/codeit/mopl/domain/notification/controller/NotificationController.java
@@ -1,5 +1,39 @@
 package com.codeit.mopl.domain.notification.controller;
 
+import com.codeit.mopl.domain.notification.dto.CursorResponseNotificationDto;
+import com.codeit.mopl.domain.notification.entity.SortBy;
+import com.codeit.mopl.domain.notification.entity.SortDirection;
+import com.codeit.mopl.domain.notification.service.NotificationService;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/notification")
 public class NotificationController {
+
+  private final NotificationService notificationService;
+
+  @GetMapping
+  public ResponseEntity<CursorResponseNotificationDto> getNotifications(
+      @RequestParam(value = "cursor", required = false) String cursor,
+      @RequestParam(value = "idAfter", required = false) UUID idAfter,
+      @RequestParam(value = "limit") int limit,
+      @RequestParam(value = "sortDirection") SortDirection sortDirection,
+      @RequestParam(value = "sortBy") SortBy sortBy
+  ) {
+    // 서비스 호출
+    CursorResponseNotificationDto response = notificationService.getNotifications(
+        cursor, idAfter, limit, sortDirection, sortBy
+    );
+    return ResponseEntity.ok(response);
+  }
 
 }

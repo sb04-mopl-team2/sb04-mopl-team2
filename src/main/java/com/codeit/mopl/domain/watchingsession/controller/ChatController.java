23f09f537878753870fe3446fe563e6ccc5e41f0
Leejun <estherleejunkim@gmail.com>
2025-11-12 15:16:29 +0900

    feat(livestream): 불필요한 컨트롤러 삭제

--- src/main/java/com/codeit/mopl/domain/watchingsession/controller/ChatController.java
+++ src/main/java/com/codeit/mopl/domain/watchingsession/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.codeit.mopl.domain.watchingsession.controller;
 import com.codeit.mopl.domain.watchingsession.dto.ContentChatDto;
 import com.codeit.mopl.domain.watchingsession.entity.ContentChatSendRequest;
 import com.codeit.mopl.domain.watchingsession.entity.UserSummary;
+import com.codeit.mopl.domain.watchingsession.entity.WatchingSessionChange;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -13,9 +14,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 
 /*
-  컨텐츠 실시간 채팅
+  컨텐츠 실시간 채팅 (WebSocket)
   - 라이브 채팅 메시지를 받아 모든 시청자에게 중계하는 컨트롤러
-  - WebSocket 사용, Service 거치지 않음
+  - Service 거치지 않음
  */
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +24,9 @@ public class ChatController {
 
   private final SimpMessagingTemplate messagingTemplate;
 
+  /**
+   * 콘텐츠 실시간 채팅
+   */
   @MessageMapping("/contents/{contentId}/chat") // adds /pub
   public void sendChat(@DestinationVariable String contentId,
       @Payload ContentChatSendRequest contentChatSendRequest,
--- src/main/java/com/codeit/mopl/domain/watchingsession/controller/WatchingSessionController.java
+++
@@ -1,8 +0,0 @@
-package com.codeit.mopl.domain.watchingsession.controller;
-
-/**
- * WatchingSessionService에 저장된 현재 상태를 조회하는 REST API 제공
- */
-public class WatchingSessionController {
-
-}

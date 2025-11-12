4b069ea9d59dc9ec9fc41cf44d375b66b5b41943
KwonYongJin <chaoskyj@gmail.com>
2025-11-12 18:39:06 +0900

    feat(notification) : 알림조회 서비스 계층 구현

--- src/main/java/com/codeit/mopl/domain/notification/controller/NotificationController.java
+++ src/main/java/com/codeit/mopl/domain/notification/controller/NotificationController.java
@@ -8,6 +8,8 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +25,7 @@ public class NotificationController {
 
   @GetMapping
   public ResponseEntity<CursorResponseNotificationDto> getNotifications(
+      @AuthenticationPrincipal UserDetails user,
       @RequestParam(value = "cursor", required = false) String cursor,
       @RequestParam(value = "idAfter", required = false) UUID idAfter,
       @RequestParam(value = "limit") int limit,
@@ -30,7 +33,12 @@ public class NotificationController {
       @RequestParam(value = "sortBy") SortBy sortBy
   ) {
     // 서비스 호출
-    CursorResponseNotificationDto response = notificationService.getNotifications(
+
+    //UUID userId = user.getId()
+    // TODO 추후 AuthenticationPrincipal 기능이 구현되면 userId를 AuthenticationPrincipal 에서 가져오도록 변경하기
+    UUID userId = UUID.randomUUID();
+
+    CursorResponseNotificationDto response = notificationService.getNotifications(userId,
         cursor, idAfter, limit, sortDirection, sortBy
     );
     return ResponseEntity.ok(response);
--- src/main/java/com/codeit/mopl/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/mopl/domain/notification/repository/NotificationRepository.java
@@ -1,9 +1,11 @@
 package com.codeit.mopl.domain.notification.repository;
 
 import com.codeit.mopl.domain.notification.entity.Notification;
+import com.codeit.mopl.domain.notification.entity.Status;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
+  Long countByReceiverIdAndStatus(UUID receiverId, Status status);
 }

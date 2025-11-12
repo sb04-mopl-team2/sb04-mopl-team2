86a58e5fb7be24fa2927f38c33cb76ab1697a996
KwonYongJin <chaoskyj@gmail.com>
2025-11-12 13:32:55 +0900

    feat(notification) : 알림 컨트롤러, 서비스 파일 생성 및 레포지터리, 매퍼 작성

--- src/main/java/com/codeit/mopl/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/mopl/domain/notification/repository/NotificationRepository.java
@@ -1,5 +1,9 @@
 package com.codeit.mopl.domain.notification.repository;
 
-public class NotificationRepository {
+import com.codeit.mopl.domain.notification.entity.Notification;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
 }
--- src/main/java/com/codeit/mopl/domain/notification/service/NotificationService.java
+++ src/main/java/com/codeit/mopl/domain/notification/service/NotificationService.java
@@ -1,5 +1,15 @@
 package com.codeit.mopl.domain.notification.service;
 
+import com.codeit.mopl.domain.notification.dto.CursorResponseNotificationDto;
+import com.codeit.mopl.domain.notification.entity.SortBy;
+import com.codeit.mopl.domain.notification.entity.SortDirection;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationService {
+  public CursorResponseNotificationDto getNotifications(String cursor, UUID idAfter, int limit, SortDirection sortDirection, SortBy sortBy) {
+    return null;
+  }
 
 }

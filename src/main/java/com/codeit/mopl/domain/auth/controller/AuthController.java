f29fb70e44d23700a05ea8874da5e5e9f1e69628
NaNHangBok <kms_1015@naver.com>
2025-11-13 10:44:01 +0900

    feat(auth): Csrf-Token 값 조회 기능 구현
    - csrf-token 값 조회 핸들러메서드 구현

--- src/main/java/com/codeit/mopl/domain/auth/controller/AuthController.java
+++ src/main/java/com/codeit/mopl/domain/auth/controller/AuthController.java
@@ -3,11 +3,24 @@ package com.codeit.mopl.domain.auth.controller;
 import com.codeit.mopl.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @GetMapping("/csrf-token")
+    public ResponseEntity<?> csrfToken(CsrfToken csrfToken) {
+        String tokenValue = csrfToken.getToken();
+        log.debug("CSRF 토큰 요청 : {}", tokenValue);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }

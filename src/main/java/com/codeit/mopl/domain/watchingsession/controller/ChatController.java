a749be3d77043570c49a1bfbb9a9c50c3db73eac
Leejun <estherleejunkim@gmail.com>
2025-11-13 09:23:58 +0900

    fix(livesession): chatcontroller에서 customuserdetails 사용

--- src/main/java/com/codeit/mopl/domain/watchingsession/controller/ChatController.java
+++ src/main/java/com/codeit/mopl/domain/watchingsession/controller/ChatController.java
@@ -1,16 +1,21 @@
 package com.codeit.mopl.domain.watchingsession.controller;
 
+import com.codeit.mopl.domain.user.dto.response.UserDto;
+import com.codeit.mopl.domain.user.entity.User;
+import com.codeit.mopl.domain.user.repository.UserRepository;
 import com.codeit.mopl.domain.watchingsession.dto.ContentChatDto;
 import com.codeit.mopl.domain.watchingsession.entity.ContentChatSendRequest;
 import com.codeit.mopl.domain.watchingsession.entity.UserSummary;
 import com.codeit.mopl.domain.watchingsession.entity.WatchingSessionChange;
+import com.codeit.mopl.exception.user.ErrorCode;
+import com.codeit.mopl.security.CustomUserDetails;
+import com.codeit.mopl.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 
 /*
@@ -24,22 +29,20 @@ public class ChatController {
 
   private final SimpMessagingTemplate messagingTemplate;
 
-  /**
-   * 콘텐츠 실시간 채팅
-   */
   @MessageMapping("/contents/{contentId}/chat") // adds /pub
   public void sendChat(@DestinationVariable String contentId,
       @Payload ContentChatSendRequest contentChatSendRequest,
-      @AuthenticationPrincipal User principal // 나중에 MoplUserDetail로 바꾸기
+      @AuthenticationPrincipal CustomUserDetails principal
   ) {
-//    UserSummary senderSummary = new UserSummary(
-//        principal.getId(),
-//        principal.getUsername(),
-//        principal.getProfileImageUrl()
-//    );
+    UserDto userDto = principal.getUser();
+
+    UserSummary senderSummary = new UserSummary(
+        userDto.id(),
+        userDto.name(),
+        userDto.profileImageUrl()
+    );
     ContentChatDto contentChatDto = new ContentChatDto(
-//        senderSummary,
-        null,
+        senderSummary,
         contentChatSendRequest.content()
     );
 

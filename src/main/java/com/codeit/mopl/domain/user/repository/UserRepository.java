b69f5cc13953ae2d5e4338fc09dce4558b08fd97
NaNHangBok <kms_1015@naver.com>
2025-11-17 15:31:06 +0900

    feat(user): 유저 목록 조회 기능 구현
    - 유저 목록 조회 기능 구현
      - 어드민 계정만 가능
    - security ExceptionHandler를 Http403 기본 Handler에서 CustomHandler로 변경
      - 401 응답 반환용 handler
      - 401 응답 시 로그아웃하는 것으로 추정

--- src/main/java/com/codeit/mopl/domain/user/repository/UserRepository.java
+++ src/main/java/com/codeit/mopl/domain/user/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> , CustomUserRepository{
 
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);

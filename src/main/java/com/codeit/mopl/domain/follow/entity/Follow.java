7bf1152b3c339ee5ae4d15423b00835f2890f64d
JihyeonLee <devlee1011@gmail.com>
2025-11-17 17:35:30 +0900

    feat(Follow): 엔티티 추가

--- src/main/java/com/codeit/mopl/domain/follow/entity/Follow.java
+++ src/main/java/com/codeit/mopl/domain/follow/entity/Follow.java
@@ -1,5 +1,33 @@
 package com.codeit.mopl.domain.follow.entity;
 
-public class Follow {
+import com.codeit.mopl.domain.base.BaseEntity;
+import com.codeit.mopl.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "follows",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_follower_followee",
+                        columnNames = {"follower_id", "followee_id"}
+                )
+        })
+@NoArgsConstructor
+public class Follow extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "follower_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "followee_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User followee;
 }

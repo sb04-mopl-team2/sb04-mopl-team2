60b410d4b658f62966053c3f242a01adb50eb9ea
KwonYongJin <chaoskyj@gmail.com>
2025-11-19 18:04:15 +0900

    refactor(notification) : Enum 파일 요소명 대문자로 변경

--- src/main/java/com/codeit/mopl/domain/review/controller/ReviewController.java
+++ src/main/java/com/codeit/mopl/domain/review/controller/ReviewController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;

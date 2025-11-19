011d3971112798a0bbba445d98748e4967af2dc6
문은서 <meunse1999@naver.com>
2025-11-19 18:42:49 +0900

    style(playlist): 에러메세지 수정

--- src/main/java/com/codeit/mopl/exception/playlist/PlaylistErrorCode.java
+++ src/main/java/com/codeit/mopl/exception/playlist/PlaylistErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum PlaylistErrorCode implements ErrorCodeInterface {
     PLAYLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "플레이리스트가 존재하지 않습니다."),
-    PLAYLIST_UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, "플레이리스트 수정 권한이 없는 유저입니다.");
+    PLAYLIST_UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, "플레이리스트 변경 권한이 없는 유저입니다.");
 
     private final HttpStatus status;
     private final String message;

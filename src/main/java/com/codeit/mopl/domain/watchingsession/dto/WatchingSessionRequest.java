b13f6d1313c2bbaf202a3145cb8adaeb7bd84451
Leejun <estherleejunkim@gmail.com>
2025-11-20 15:35:52 +0900

    fix(watchingsession): sortby enum 대문자

--- src/main/java/com/codeit/mopl/domain/watchingsession/dto/WatchingSessionRequest.java
+++ src/main/java/com/codeit/mopl/domain/watchingsession/dto/WatchingSessionRequest.java
@@ -2,6 +2,7 @@ package com.codeit.mopl.domain.watchingsession.dto;
 
 import com.codeit.mopl.domain.watchingsession.entity.enums.SortBy;
 import com.codeit.mopl.domain.watchingsession.entity.enums.SortDirection;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
@@ -10,7 +11,7 @@ public record WatchingSessionRequest(
     String watcherNameLike,
     String cursor,
     UUID idAfter,
-    @Min(1)
+    @Min(1) @Max(100)
     int limit,
     @NotNull
     SortDirection sortDirection,

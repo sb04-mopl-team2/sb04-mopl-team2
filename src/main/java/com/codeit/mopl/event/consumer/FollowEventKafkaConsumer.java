aaabd517f95b226740c8249bf64813518ad46068
KwonYongJin <chaoskyj@gmail.com>
2025-11-21 14:37:42 +0900

    refactor(Kafka) : 카프카 멱등성 보장 코드 추가

--- src/main/java/com/codeit/mopl/event/consumer/FollowEventKafkaConsumer.java
+++ src/main/java/com/codeit/mopl/event/consumer/FollowEventKafkaConsumer.java
@@ -2,11 +2,15 @@ package com.codeit.mopl.event.consumer;
 
 import com.codeit.mopl.domain.follow.dto.FollowDto;
 import com.codeit.mopl.domain.follow.service.FollowService;
+import com.codeit.mopl.event.entity.EventType;
+import com.codeit.mopl.event.entity.ProcessedEvent;
 import com.codeit.mopl.event.event.FollowerIncreaseEvent;
+import com.codeit.mopl.event.repository.ProcessedEventRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
@@ -18,12 +22,22 @@ public class FollowEventKafkaConsumer {
 
     private final FollowService followService;
     private final ObjectMapper objectMapper;
+    private final ProcessedEventRepository processedEventRepository;
 
     @KafkaListener(topics = "mopl-follower-increase", groupId = "mopl-follow", concurrency = "3")
     public void onFollowIncrease(String kafkaEventJson, Acknowledgment ack) {
         try {
             FollowerIncreaseEvent event = objectMapper.readValue(kafkaEventJson, FollowerIncreaseEvent.class);
             FollowDto followDto = event.followDto();
+
+            try {
+                processedEventRepository.save(new ProcessedEvent(followDto.id(), EventType.NOTIFICATION_CREATED));
+            } catch (DataIntegrityViolationException e) {
+                log.warn("[Kafka] 이미 처리된 팔로워 증가 이벤트입니다. eventId={}", followDto.id());
+                ack.acknowledge();
+                return;
+            }
+
             followService.increaseFollowerCount(followDto);
             ack.acknowledge();
         } catch (JsonProcessingException e) {
---
+++ src/main/java/com/codeit/mopl/event/repository/ProcessedEventRepository.java
@@ -0,0 +1,9 @@
+package com.codeit.mopl.event.repository;
+
+import com.codeit.mopl.domain.follow.entity.Follow;
+import com.codeit.mopl.event.entity.ProcessedEvent;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, UUID> {
+}

a94761bb653df5a0261b57c0319e96937867faa3
JihyeonLee <devlee1011@gmail.com>
2025-11-21 11:04:16 +0900

    refactor(Follow): FolloweeIdIsNull 에러 코드 메시지 변경
    - 에러 메시지 더 간단하게 변경

--- src/main/java/com/codeit/mopl/exception/follow/FollowErrorCode.java
+++ src/main/java/com/codeit/mopl/exception/follow/FollowErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum FollowErrorCode implements ErrorCodeInterface {
     FOLLOW_SELF_PROHIBITED(HttpStatus.BAD_REQUEST, "자기 자신을 팔로우할 수 없습니다."),
     FOLLOW_DUPLICATE(HttpStatus.BAD_REQUEST, "같은 사용자를 중복해서 팔로우할 수 없습니다."),
-    FOLLOWEE_ID_IS_NULL(HttpStatus.BAD_REQUEST, "FolloweeId가 null일 수 없습니다.");
+    FOLLOWEE_ID_IS_NULL(HttpStatus.BAD_REQUEST, "FolloweeId는 필수 값입니다.");
 
     private final HttpStatus status;
     private final String message;

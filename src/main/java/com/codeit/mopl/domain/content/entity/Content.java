6224672ce00746c9880deeec23edbb717b6584ae
JihyeonLee <devlee1011@gmail.com>
2025-11-24 17:25:26 +0900

    refactor(Content): description 타입 변경: CLOB -> TEXT

--- src/main/java/com/codeit/mopl/domain/content/entity/Content.java
+++ src/main/java/com/codeit/mopl/domain/content/entity/Content.java
@@ -1,20 +1,13 @@
 package com.codeit.mopl.domain.content.entity;
 
 import com.codeit.mopl.domain.base.UpdatableEntity;
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,8 +20,7 @@ public class Content extends UpdatableEntity {
   private String title;
 
   @NotNull
-  @Lob
-  @Column(nullable = false)
+  @Column(nullable = false, columnDefinition = "TEXT")
   private String description;
 
   private String thumbnailUrl;

fe1c8a9c8c7df01306f3ba755fb3a9e892e1ac87
Oh-Myeongjae <omjcws@gmail.com>
2025-11-24 13:31:24 +0900

    refactor(Content): 콘텐츠 코드리뷰 반영 리팩토링

--- src/main/java/com/codeit/mopl/exception/global/GlobalExceptionHandler.java
+++ src/main/java/com/codeit/mopl/exception/global/GlobalExceptionHandler.java
@@ -1,18 +1,19 @@
 package com.codeit.mopl.exception.global;
 
 import jakarta.mail.MessagingException;
+import java.time.LocalDateTime;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.time.LocalDateTime;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
   @ExceptionHandler(MoplException.class)
   public ResponseEntity<ErrorResponse> handleMoplException(MoplException e) {
     log.error(e.getMessage(), e);
@@ -29,6 +30,17 @@ public class GlobalExceptionHandler {
     ErrorResponse errorResponse = new ErrorResponse("MessagingException", e.getMessage(), Map.of("log","이메일 발신 중 오류 발생"), LocalDateTime.now());
 
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+    log.error(e.getMessage(), e);
+
+    ErrorCodeInterface errorCode = ErrorCode.INVALID_INPUT_VALUE;
+
+    ErrorResponse errorResponse = new ErrorResponse(errorCode.getName(), errorCode.getMessage() , Map.of("입력값 검증 실패","유효하지 않은 입력값입니다."), LocalDateTime.now());
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
   }
 
 }

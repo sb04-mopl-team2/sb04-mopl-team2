c5bc90281a6821c68090d5843e274fa4d8eb7190
JihyeonLee <devlee1011@gmail.com>
2025-11-24 14:12:23 +0900

    feat(Follow): 팔로우 API 슬라이스 테스트 1차 구현
    - 에러 해결 필요

---
+++ src/test/java/com/codeit/mopl/domain/follow/controller/FollowControllerTest.java
@@ -0,0 +1,112 @@
+package com.codeit.mopl.domain.follow.controller;
+
+import com.codeit.mopl.domain.follow.dto.FollowDto;
+import com.codeit.mopl.domain.follow.dto.FollowRequest;
+import com.codeit.mopl.domain.follow.service.FollowService;
+import com.codeit.mopl.domain.user.dto.response.UserDto;
+import com.codeit.mopl.domain.user.entity.Role;
+import com.codeit.mopl.security.CustomUserDetails;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FollowController.class)
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+@ActiveProfiles("test")
+class FollowControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private FollowService followService;
+
+    @Test
+    @DisplayName("팔로우 성공 테스트")
+    void follow_Success() throws Exception {
+        // given
+        UUID followId = UUID.randomUUID();
+        UUID followeeId = UUID.randomUUID();
+        UUID followerId = UUID.randomUUID();
+        UserDto userDto = new UserDto(
+                followerId,
+                LocalDateTime.now(),
+                "testUser@test.com",
+                "test",
+                null,
+                Role.USER,
+                false
+        );
+        CustomUserDetails follower = new CustomUserDetails(userDto, "test1234");
+        FollowRequest request = new FollowRequest(followeeId);
+        String content = objectMapper.writeValueAsString(request);
+
+        FollowDto followDto = new FollowDto(
+                followId,
+                followerId,
+                followeeId
+        );
+
+        given(followService.createFollow(eq(request), eq(followerId))).willReturn(followDto);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post("/api/follows")
+                        .with(user(follower))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+        // then
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(followId.toString()))
+                .andExpect(jsonPath("$.followerId").value(followerId.toString()))
+                .andExpect(jsonPath("$.followeeId").value(followeeId.toString()));
+    }
+
+    @Test
+    @DisplayName("팔로우 실패 테스트 - 유효하지 않은 요청")
+    void follow_Failure_InvalidRequest() throws Exception {
+        // given
+
+        // when & then
+
+    }
+
+    @Test
+    @DisplayName("팔로우 실패 테스트 - 인증되지 않은 사용자")
+    void follow_Failure_Authentication_Failed() throws Exception {
+        // given
+
+        // when & then
+
+    }
+
+}

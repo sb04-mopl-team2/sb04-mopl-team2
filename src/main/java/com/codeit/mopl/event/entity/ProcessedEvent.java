1394ea92a9b1c0966594906f27c6c44b2e17d650
KwonYongJin <chaoskyj@gmail.com>
2025-11-24 11:04:14 +0900

    feat(kafka) : ProcessedEvent 엔티티 수정

--- src/main/java/com/codeit/mopl/event/entity/ProcessedEvent.java
+++ src/main/java/com/codeit/mopl/event/entity/ProcessedEvent.java
@@ -1,22 +1,16 @@
 package com.codeit.mopl.event.entity;
 
+import com.codeit.mopl.domain.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import java.time.LocalDateTime;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -25,19 +19,14 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Table(name = "processed_events")
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
-public class ProcessedEvent {
+public class ProcessedEvent extends BaseEntity {
 
-  @Id
   @Column(name = "event_id", nullable = false, updatable = false)
   private UUID eventId;
 
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private EventType eventType;
-
-  @CreatedDate
-  @Column(name = "eventTime", updatable = false, nullable = false)
-  private LocalDateTime eventTime;
 
   public ProcessedEvent(UUID id, EventType eventType) {
     this.eventId = id;

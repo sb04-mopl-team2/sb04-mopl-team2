0ccaae3ced7680ed3d4541812c0382b3c27506fa
문은서 <meunse1999@naver.com>
2025-11-25 14:26:19 +0900

    feat(message): 예외코드 추가

--- src/main/java/com/codeit/mopl/exception/message/MessageErrorCode.java
+++ src/main/java/com/codeit/mopl/exception/message/MessageErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MessageErrorCode implements ErrorCodeInterface {
-    CONVERSATION_ALREADY_EXIST(HttpStatus.CONFLICT,"동일한 채팅방이 이미 존재합니다.");
+    CONVERSATION_ALREADY_EXIST(HttpStatus.CONFLICT,"동일한 채팅방이 이미 존재합니다."),
+    CONVERSATION_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 채팅방이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;

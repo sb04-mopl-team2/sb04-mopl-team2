d8a99d855d55f02baf285f1d66e15e41b71bcaaf
KwonYongJin <chaoskyj@gmail.com>
2025-11-25 16:39:47 +0900

    test(review) : 리뷰 통합 테스트 작성

--- src/main/java/com/codeit/mopl/exception/review/ReviewDuplicateException.java
+++ src/main/java/com/codeit/mopl/exception/review/ReviewDuplicateException.java
@@ -1,7 +1,10 @@
 package com.codeit.mopl.exception.review;
 
 import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class ReviewDuplicateException extends ReviewException {
 
     public ReviewDuplicateException(ReviewErrorCode reviewErrorCode, Map<String, Object> details) {

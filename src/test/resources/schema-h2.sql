270b66172240999f184396482df6d02fd2738a7a
NaNHangBok <kms_1015@naver.com>
2025-11-25 14:04:56 +0900

    Revert "test(test): test용 h2 스키마 변경"
    This reverts commit 566de376f9eaa5f88c9a1f1764edee75059fce89.

--- src/test/resources/schema-h2.sql
+++ src/test/resources/schema-h2.sql
@@ -1,7 +1,7 @@
 -- USERS TABLE
 CREATE TABLE IF NOT EXISTS users
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     email VARCHAR(255) UNIQUE NOT NULL,
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS users
     role VARCHAR(20) NOT NULL,
     locked BOOLEAN NOT NULL DEFAULT FALSE,
     follower_count BIGINT NOT NULL DEFAULT 0
-);
+    );
 
 -- CONTENTS TABLE
 CREATE TABLE IF NOT EXISTS contents
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     title VARCHAR(255) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS contents
     average_rating DOUBLE DEFAULT 0.0,
     review_count INT DEFAULT 0,
     watcher_count INT DEFAULT 0
-);
+    );
 
 -- CONTENT TAGS TABLE
 CREATE TABLE IF NOT EXISTS contents_tags
@@ -36,12 +36,12 @@ CREATE TABLE IF NOT EXISTS contents_tags
 
     PRIMARY KEY (content_id, tag),
     FOREIGN KEY (content_id) REFERENCES contents(id) ON DELETE CASCADE
-);
+    );
 
 -- PLAYLISTS TABLE
 CREATE TABLE IF NOT EXISTS playlists
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     owner_id UUID NOT NULL,
@@ -51,24 +51,24 @@ CREATE TABLE IF NOT EXISTS playlists
     subscribe_by_me BOOLEAN NOT NULL DEFAULT FALSE,
 
     FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
-);
+    );
 
 -- PLAYLIST ITEMS TABLE
 CREATE TABLE IF NOT EXISTS playlist_items
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     playlist_id UUID NOT NULL,
     content_id UUID NOT NULL,
 
     FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE,
     FOREIGN KEY (content_id) REFERENCES contents(id) ON DELETE CASCADE
-);
+    );
 
 -- PLAYLIST SUBSCRIPTIONS TABLE
 CREATE TABLE IF NOT EXISTS playlist_subscriptions
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     subscriber_id UUID NOT NULL,
@@ -77,12 +77,12 @@ CREATE TABLE IF NOT EXISTS playlist_subscriptions
     FOREIGN KEY (subscriber_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE,
     UNIQUE (subscriber_id, playlist_id)
-);
+    );
 
 -- NOTIFICATIONS TABLE
 CREATE TABLE IF NOT EXISTS notifications
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
@@ -93,16 +93,15 @@ CREATE TABLE IF NOT EXISTS notifications
     status VARCHAR(20) NOT NULL DEFAULT 'UNREAD',
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+    );
 
 -- REVIEWS TABLE
 CREATE TABLE IF NOT EXISTS reviews
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
-    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
     text TEXT NOT NULL,
     rating DOUBLE NOT NULL DEFAULT 0.0,
     content_id UUID NOT NULL,
@@ -110,12 +109,12 @@ CREATE TABLE IF NOT EXISTS reviews
 
     FOREIGN KEY (content_id) REFERENCES contents(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+    );
 
 -- CONVERSATIONS TABLE
 CREATE TABLE IF NOT EXISTS conversations
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     with_user_id UUID NOT NULL,
@@ -124,12 +123,12 @@ CREATE TABLE IF NOT EXISTS conversations
 
     FOREIGN KEY (with_user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+    );
 
 -- DIRECT MESSAGES TABLE
 CREATE TABLE IF NOT EXISTS direct_messages
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     sender UUID NOT NULL,
@@ -140,12 +139,12 @@ CREATE TABLE IF NOT EXISTS direct_messages
     FOREIGN KEY (sender) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (receiver) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (conversation_id) REFERENCES conversations(id) ON DELETE CASCADE
-);
+    );
 
 -- FOLLOWS TABLE
 CREATE TABLE IF NOT EXISTS follows
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     follower_id UUID NOT NULL,
     followee_id UUID NOT NULL,
@@ -153,12 +152,12 @@ CREATE TABLE IF NOT EXISTS follows
     FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (followee_id) REFERENCES users(id) ON DELETE CASCADE,
     UNIQUE (follower_id, followee_id)
-);
+    );
 
 -- WATCHING SESSION TABLE
 CREATE TABLE IF NOT EXISTS watching_sessions
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
@@ -168,15 +167,100 @@ CREATE TABLE IF NOT EXISTS watching_sessions
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (content_id) REFERENCES contents(id) ON DELETE CASCADE,
     UNIQUE(user_id)
-);
+    );
 
 -- PROCESSED EVENT
 CREATE TABLE IF NOT EXISTS processed_events
 (
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     event_id UUID NOT NULL,
     event_type VARCHAR(255) NOT NULL,
 
     UNIQUE(event_id)
-);
+    );
+
+ALTER TABLE follows
+    ADD CONSTRAINT no_self_follow CHECK (follower_id != followee_id);
+
+
+
+-- Autogenerated: do not edit this file
+
+CREATE TABLE BATCH_JOB_INSTANCE  (
+                                     JOB_INSTANCE_ID BIGINT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY ,
+                                     VERSION BIGINT ,
+                                     JOB_NAME VARCHAR(100) NOT NULL,
+                                     JOB_KEY VARCHAR(32) NOT NULL,
+                                     constraint JOB_INST_UN unique (JOB_NAME, JOB_KEY)
+) ;
+
+CREATE TABLE BATCH_JOB_EXECUTION  (
+                                      JOB_EXECUTION_ID BIGINT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY ,
+                                      VERSION BIGINT  ,
+                                      JOB_INSTANCE_ID BIGINT NOT NULL,
+                                      CREATE_TIME TIMESTAMP(9) NOT NULL,
+                                      START_TIME TIMESTAMP(9) DEFAULT NULL ,
+                                      END_TIME TIMESTAMP(9) DEFAULT NULL ,
+                                      STATUS VARCHAR(10) ,
+                                      EXIT_CODE VARCHAR(2500) ,
+                                      EXIT_MESSAGE VARCHAR(2500) ,
+                                      LAST_UPDATED TIMESTAMP(9),
+                                      constraint JOB_INST_EXEC_FK foreign key (JOB_INSTANCE_ID)
+                                          references BATCH_JOB_INSTANCE(JOB_INSTANCE_ID)
+) ;
+
+CREATE TABLE BATCH_JOB_EXECUTION_PARAMS  (
+                                             JOB_EXECUTION_ID BIGINT NOT NULL ,
+                                             PARAMETER_NAME VARCHAR(100) NOT NULL ,
+                                             PARAMETER_TYPE VARCHAR(100) NOT NULL ,
+                                             PARAMETER_VALUE VARCHAR(2500) ,
+                                             IDENTIFYING CHAR(1) NOT NULL ,
+                                             constraint JOB_EXEC_PARAMS_FK foreign key (JOB_EXECUTION_ID)
+                                                 references BATCH_JOB_EXECUTION(JOB_EXECUTION_ID)
+) ;
+
+CREATE TABLE BATCH_STEP_EXECUTION  (
+                                       STEP_EXECUTION_ID BIGINT GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY ,
+                                       VERSION BIGINT NOT NULL,
+                                       STEP_NAME VARCHAR(100) NOT NULL,
+                                       JOB_EXECUTION_ID BIGINT NOT NULL,
+                                       CREATE_TIME TIMESTAMP(9) NOT NULL,
+                                       START_TIME TIMESTAMP(9) DEFAULT NULL ,
+                                       END_TIME TIMESTAMP(9) DEFAULT NULL ,
+                                       STATUS VARCHAR(10) ,
+                                       COMMIT_COUNT BIGINT ,
+                                       READ_COUNT BIGINT ,
+                                       FILTER_COUNT BIGINT ,
+                                       WRITE_COUNT BIGINT ,
+                                       READ_SKIP_COUNT BIGINT ,
+                                       WRITE_SKIP_COUNT BIGINT ,
+                                       PROCESS_SKIP_COUNT BIGINT ,
+                                       ROLLBACK_COUNT BIGINT ,
+                                       EXIT_CODE VARCHAR(2500) ,
+                                       EXIT_MESSAGE VARCHAR(2500) ,
+                                       LAST_UPDATED TIMESTAMP(9),
+                                       constraint JOB_EXEC_STEP_FK foreign key (JOB_EXECUTION_ID)
+                                           references BATCH_JOB_EXECUTION(JOB_EXECUTION_ID)
+) ;
+
+CREATE TABLE BATCH_STEP_EXECUTION_CONTEXT  (
+                                               STEP_EXECUTION_ID BIGINT NOT NULL PRIMARY KEY,
+                                               SHORT_CONTEXT VARCHAR(2500) NOT NULL,
+                                               SERIALIZED_CONTEXT LONGVARCHAR ,
+                                               constraint STEP_EXEC_CTX_FK foreign key (STEP_EXECUTION_ID)
+                                                   references BATCH_STEP_EXECUTION(STEP_EXECUTION_ID)
+) ;
+
+CREATE TABLE BATCH_JOB_EXECUTION_CONTEXT  (
+                                              JOB_EXECUTION_ID BIGINT NOT NULL PRIMARY KEY,
+                                              SHORT_CONTEXT VARCHAR(2500) NOT NULL,
+                                              SERIALIZED_CONTEXT LONGVARCHAR ,
+                                              constraint JOB_EXEC_CTX_FK foreign key (JOB_EXECUTION_ID)
+                                                  references BATCH_JOB_EXECUTION(JOB_EXECUTION_ID)
+) ;
+
+CREATE SEQUENCE BATCH_STEP_EXECUTION_SEQ;
+CREATE SEQUENCE BATCH_JOB_EXECUTION_SEQ;
+CREATE SEQUENCE BATCH_JOB_SEQ;
+

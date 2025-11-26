b5c9fc9077111b5c45b0631b816b4a4176588ee4
KwonYongJin <chaoskyj@gmail.com>
2025-11-26 14:06:21 +0900

    test(notification) : 알림 테스트 코드 추가

--- src/test/java/com/codeit/mopl/domain/notification/repository/NotificationRepositoryImplTest.java
+++ src/test/java/com/codeit/mopl/domain/notification/repository/NotificationRepositoryImplTest.java
@@ -34,6 +34,8 @@ public class NotificationRepositoryImplTest {
   private Notification n2;
   private Notification n3;
   private Notification n4;
+  private Notification n5;
+  private Notification n6;
 
   @Autowired
   private NotificationRepository notificationRepository;
@@ -49,18 +51,27 @@ public class NotificationRepositoryImplTest {
 
     n1 = createNotification(user, "testTitle1", "testContent1", Level.INFO, Status.UNREAD);
     em.persist(n1);
-    Thread.sleep(5);
+    Thread.sleep(300);
 
     n2 = createNotification(user, "testTitle2", "testContent2", Level.INFO, Status.UNREAD);
     em.persist(n2);
-    Thread.sleep(5);
+    Thread.sleep(300);
 
     n3 = createNotification(user, "testTitle3", "testContent3", Level.INFO, Status.READ);
     em.persist(n3);
-    Thread.sleep(5);
+    Thread.sleep(300);
 
     n4 = createNotification(user, "testTitle4", "testContent4", Level.INFO, Status.UNREAD);
     em.persist(n4);
+    Thread.sleep(300);
+
+    n5 = createNotification(user, "testTitle5", "testContent5", Level.INFO, Status.UNREAD);
+    em.persist(n5);
+    Thread.sleep(300);
+
+    n6 = createNotification(user, "testTitle6", "testContent6", Level.INFO, Status.UNREAD);
+    em.persist(n6);
+    Thread.sleep(300);
 
     em.flush();
     em.clear();
@@ -77,15 +88,15 @@ public class NotificationRepositoryImplTest {
         userId,
         null,
         null,
-        10,
+        3,
         SortDirection.DESCENDING,
         SortBy.CREATED_AT
     );
 
     // then
     assertThat(result.get(0).getId())
-        .isEqualTo(n4.getId());
-    assertThat(result.size()).isEqualTo(3);
+        .isEqualTo(n6.getId());
+    assertThat(result.size()).isEqualTo(4);
   }
 
   @Test
@@ -99,7 +110,7 @@ public class NotificationRepositoryImplTest {
         userId,
         null,
         null,
-        10,
+        3,
         SortDirection.ASCENDING,
         SortBy.CREATED_AT
     );
@@ -108,7 +119,59 @@ public class NotificationRepositoryImplTest {
     assertThat(result.get(0).getId())
         .isEqualTo(n1.getId());
 
-    assertThat(result.size()).isEqualTo(3);
+    assertThat(result.size()).isEqualTo(4);
+  }
+
+  @Test
+  @DisplayName("cursor와 DESC 정렬이 주어지면, 해당 시간 이전의 UNREAD 알림만 조회된다")
+  void searchNotification_withCursorAndDesc() {
+    // given
+    UUID userId = user.getId();
+
+    String cursor = n4.getCreatedAt().toString();
+
+    // when
+    List<Notification> result = notificationRepository.searchNotifications(
+        userId,
+        cursor,
+        n4.getId(),
+        3,
+        SortDirection.DESCENDING,
+        SortBy.CREATED_AT
+    );
+
+    // then
+    assertThat(result)
+        .extracting(Notification::getId)
+        .containsExactly(n2.getId(), n1.getId());
+
+    assertThat(result.size()).isEqualTo(2);
+  }
+
+  @Test
+  @DisplayName("cursor와 ASC 정렬이 주어지면, 해당 시간 이후의 UNREAD 알림만 조회된다")
+  void searchNotification_withCursorAndAsc() {
+    // given
+    UUID userId = user.getId();
+
+    String cursor = n4.getCreatedAt().toString();
+
+    // when
+    List<Notification> result = notificationRepository.searchNotifications(
+        userId,
+        cursor,
+        n4.getId(),
+        3,
+        SortDirection.ASCENDING,
+        SortBy.CREATED_AT
+    );
+
+    // then
+    assertThat(result)
+        .extracting(Notification::getId)
+        .containsExactly(n5.getId(), n6.getId());
+
+    assertThat(result.size()).isEqualTo(2);
   }
 
   private Notification createNotification(User user, String title, String content, Level level, Status status) {

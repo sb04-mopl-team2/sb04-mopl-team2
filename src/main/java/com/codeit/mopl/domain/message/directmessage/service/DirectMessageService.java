81073629007e7008093645318a9db154bd7520f2
문은서 <meunse1999@naver.com>
2025-11-27 14:55:28 +0900

    feat(message): dm 목록 조회 서비스 코드 추가

--- src/main/java/com/codeit/mopl/domain/message/directmessage/service/DirectMessageService.java
+++ src/main/java/com/codeit/mopl/domain/message/directmessage/service/DirectMessageService.java
@@ -1,7 +1,93 @@
 package com.codeit.mopl.domain.message.directmessage.service;
 
+import com.codeit.mopl.domain.message.conversation.dto.request.ConversationSearchCond;
+import com.codeit.mopl.domain.message.conversation.entity.SortBy;
+import com.codeit.mopl.domain.message.conversation.repository.ConversationRepository;
+import com.codeit.mopl.domain.message.directmessage.dto.CursorResponseDirectMessageDto;
+import com.codeit.mopl.domain.message.directmessage.dto.DirectMessageDto;
+import com.codeit.mopl.domain.message.directmessage.dto.DirectMessageSearchCond;
+import com.codeit.mopl.domain.message.directmessage.entity.DirectMessage;
+import com.codeit.mopl.domain.message.directmessage.mapper.DirectMessageMapper;
+import com.codeit.mopl.domain.message.directmessage.repository.DirectMessageRepository;
+import com.codeit.mopl.domain.notification.entity.SortDirection;
+import com.codeit.mopl.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class DirectMessageService {
+    private final DirectMessageRepository directMessageRepository;
+    private final UserService userService;
+    private final ConversationRepository conversationRepository;
+    private final DirectMessageMapper directMessageMapper;
+
+    @Transactional(readOnly = true)
+    public CursorResponseDirectMessageDto getDirectMessages(UUID loginUserId,
+                                                            UUID conversationId,
+                                                            DirectMessageSearchCond cond) {
+        log.info("[메세지] 해당 채팅방의 DM 목록 조회 시작 - loginUserId = {} conversation {}", loginUserId, conversationId);
+        List<DirectMessage> directMessages;
+        if (cond.getSortDirection() ==SortDirection.DESCENDING) {
+            directMessages = directMessageRepository.findMessagesBefore(
+                    conversationId,
+                    cond.getCursor(),
+                    cond.getIdAfter(),
+                    cond.getSortDirection()
+            );
+        } else {
+            directMessages = directMessageRepository.findMessagesAfter(
+                    conversationId,
+                    cond.getCursor(),
+                    cond.getIdAfter(),
+                    cond.getSortDirection()
+            );
+        }
+        //빈 리스트 체크
+        if (directMessages.isEmpty()) {
+            return new CursorResponseDirectMessageDto(
+                    new ArrayList<>(),
+                    null,
+                    null,
+                    false,
+                    0L,
+                    cond.getSortBy(),
+                    cond.getSortDirection()
+            );
+        }
+        Integer limit = cond.getLimit() + 1;
+        int originalSize = directMessages.size();
+        boolean hasNext = originalSize > limit;
+        List<DirectMessage> result = hasNext
+                ? directMessages.subList(0, limit)
+                : directMessages;
+        DirectMessage last = result.get(result.size() - 1);
+        String nextCursor = hasNext ? last.getCreatedAt().toString() : null;
+        UUID nextAfter = hasNext ? last.getId() : null;
+
+        List<DirectMessageDto> directMessageDtos =
+                result.stream()
+                        .map(directMessageMapper::toDirectMessageDto)
+                        .collect(Collectors.toList());
+        long totalCount = directMessageRepository.countAllByConversationId(conversationId);
+        log.info("[메세지] 해당 채팅방의 DM 목록 조회 완료 - conversationId = {}, totalCount = {}", conversationId, totalCount);
+        return new CursorResponseDirectMessageDto(
+                directMessageDtos,
+                nextCursor,
+                nextAfter,
+                hasNext,
+                totalCount,
+                cond.getSortBy(),
+                cond.getSortDirection()
+        );
+    }
 }

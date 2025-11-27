787971af97b43cc35a9f605d7d4c4ef05b26b3a7
문은서 <meunse1999@naver.com>
2025-11-28 01:51:42 +0900

    feat(message): dm 저장 서비스코드 및 dto추가
    - DirectMessageSendRequest

--- src/main/java/com/codeit/mopl/domain/message/directmessage/service/DirectMessageService.java
+++ src/main/java/com/codeit/mopl/domain/message/directmessage/service/DirectMessageService.java
@@ -1,15 +1,22 @@
 package com.codeit.mopl.domain.message.directmessage.service;
 
 
+import com.codeit.mopl.domain.message.conversation.entity.Conversation;
 import com.codeit.mopl.domain.message.conversation.repository.ConversationRepository;
 import com.codeit.mopl.domain.message.directmessage.dto.CursorResponseDirectMessageDto;
 import com.codeit.mopl.domain.message.directmessage.dto.DirectMessageDto;
 import com.codeit.mopl.domain.message.directmessage.dto.DirectMessageSearchCond;
+import com.codeit.mopl.domain.message.directmessage.dto.DirectMessageSendRequest;
 import com.codeit.mopl.domain.message.directmessage.entity.DirectMessage;
 import com.codeit.mopl.domain.message.directmessage.mapper.DirectMessageMapper;
 import com.codeit.mopl.domain.message.directmessage.repository.DirectMessageRepository;
 import com.codeit.mopl.domain.notification.entity.SortDirection;
+import com.codeit.mopl.domain.user.entity.User;
+import com.codeit.mopl.domain.user.repository.UserRepository;
 import com.codeit.mopl.domain.user.service.UserService;
+import com.codeit.mopl.exception.message.conversation.ConversationNotFound;
+import com.codeit.mopl.exception.user.UserErrorCode;
+import com.codeit.mopl.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -27,6 +35,9 @@ import java.util.stream.Collectors;
 public class DirectMessageService {
     private final DirectMessageRepository directMessageRepository;
     private final DirectMessageMapper directMessageMapper;
+    private final ConversationRepository conversationRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public CursorResponseDirectMessageDto getDirectMessages(UUID loginUserId,
@@ -83,5 +94,25 @@ public class DirectMessageService {
                 cond.getSortBy(),
                 cond.getSortDirection()
         );
+    }
+
+    public DirectMessageDto saveDirectMessage(UUID loginUserId,
+                                              DirectMessageSendRequest request
+                                              ){
+        Conversation conversation = conversationRepository.findById(request.conversationId())
+                .orElseThrow(() -> ConversationNotFound.withId(request.receiverId()));
+        User sender = userRepository.findById(loginUserId)
+                .orElseThrow(()-> new  UserNotFoundException(UserErrorCode.USER_NOT_FOUND, Map.of("userId", loginUserId)));
+        User receiver = userRepository.findById(request.receiverId())
+                .orElseThrow(()-> new  UserNotFoundException(UserErrorCode.USER_NOT_FOUND, Map.of("userId", request.receiverId())));
+        DirectMessage directMessage = directMessageRepository.save(DirectMessage.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .conversation(conversation)
+                .content(request.content())
+                .isRead(false)
+                .build()
+        );
+        return directMessageMapper.toDirectMessageDto(directMessage);
     }
 }

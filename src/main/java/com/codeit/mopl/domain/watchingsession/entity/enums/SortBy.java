b5c4d5eb820e714c8b4ff478b8e5b38a97e15492
Leejun <estherleejunkim@gmail.com>
2025-12-01 11:14:36 +0900

    fix(watchingsession): sortby 오류 해결

--- src/main/java/com/codeit/mopl/domain/watchingsession/entity/enums/SortBy.java
+++ src/main/java/com/codeit/mopl/domain/watchingsession/entity/enums/SortBy.java
@@ -1,5 +1,11 @@
 package com.codeit.mopl.domain.watchingsession.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SortBy {
-  CREATED_AT
+  CREATED_AT("createdAt");
+  private final String type;
 }

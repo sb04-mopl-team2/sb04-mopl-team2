1ed5fe7ac67b2670e20d2669190892f2f5e94dcc
문은서 <meunse1999@naver.com>
2025-12-03 01:42:06 +0900

    fix(message): 첫페이지 조회 추가
    - DM 조회 시 첫 페이지 조회, cursor == null일 경우에 대한 분기 추가
    - 쿼리 메서드 findFirstPage추가

--- src/main/java/com/codeit/mopl/domain/message/directmessage/service/DirectMessageService.java
+++ src/main/java/com/codeit/mopl/domain/message/directmessage/service/DirectMessageService.java
@@ -19,6 +19,7 @@ import com.codeit.mopl.exception.user.UserErrorCode;
 import com.codeit.mopl.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,38 +59,86 @@ public class DirectMessageService {
 
         List<DirectMessage> directMessages;
         Pageable pageable = PageRequest.of(0, cond.getLimit() + 1);
-        LocalDateTime cursor = null;
-        if (cond.getCursor() != null) {
-            cursor = LocalDateTime.parse(cond.getCursor());
+//        LocalDateTime cursor = null;
+//        if (cond.getCursor() != null) {
+//            cursor = LocalDateTime.parse(cond.getCursor());
+//        }
+//        if (cond.getSortDirection() ==SortDirection.DESCENDING) {
+//            directMessages = directMessageRepository.findMessagesBefore(
+//                    conversationId,
+//                    cursor,
+//                    cond.getIdAfter(),
+//                    pageable
+//            );
+//        } else {
+//            directMessages = directMessageRepository.findMessagesAfter(
+//                    conversationId,
+//                    cursor,
+//                    cond.getIdAfter(),
+//                    pageable
+//            );
+
+        // 첫 페이지 조회(cursor 없는 조회)
+        if (cond.getCursor() == null) {
+            directMessages = directMessageRepository.findFirstPage(conversationId, pageable);
+            // dm 목록이 비어있는지 체크
+            if (directMessages.isEmpty()) {
+                return new CursorResponseDirectMessageDto(
+                        new ArrayList<>(),
+                        null,
+                        null,
+                        false,
+                        0L,
+                        cond.getSortBy(),
+                        cond.getSortDirection()
+                );
+            }
+            long totalCount = directMessageRepository.countAllByConversationId(conversationId);
+            boolean hasNext = directMessages.size() > cond.getLimit();
+            List<DirectMessage> result = hasNext
+                    ? directMessages.subList(0, cond.getLimit())
+                    : directMessages;
+            DirectMessage last = result.get(result.size() - 1);
+            String nextCursor = hasNext ? last.getCreatedAt().toString() : null;
+            UUID nextAfter = hasNext ? last.getId() : null;
+
+            List<DirectMessageDto> directMessageDtos =
+                    result.stream()
+                            .map(directMessageMapper::toDirectMessageDto)
+                            .collect(Collectors.toList());
+            log.info("[메세지] 해당 채팅방의 DM 목록 조회 완료 - conversationId = {}, totalCount = {}", conversationId, totalCount);
+                return new CursorResponseDirectMessageDto(
+                        directMessageDtos,
+                        null,
+                        null,
+                        hasNext,
+                        totalCount,
+                        cond.getSortBy(),
+                        cond.getSortDirection()
+                );
+
         }
-        if (cond.getSortDirection() ==SortDirection.DESCENDING) {
-            directMessages = directMessageRepository.findMessagesBefore(
-                    conversationId,
-                    cursor,
-                    cond.getIdAfter(),
-                    pageable
-            );
-        } else {
-            directMessages = directMessageRepository.findMessagesAfter(
-                    conversationId,
-                    cursor,
-                    cond.getIdAfter(),
-                    pageable
-            );
-        }
+            LocalDateTime cursor = LocalDateTime.parse(cond.getCursor());
+            if (cond.getSortDirection() == SortDirection.DESCENDING) {
+                directMessages = directMessageRepository.findMessagesBefore(
+                        conversationId,
+                        cursor,
+                        cond.getIdAfter(),
+                        pageable
+                );
+            } else {
+                directMessages = directMessageRepository.findMessagesAfter(
+                        conversationId,
+                        cursor,
+                        cond.getIdAfter(),
+                        pageable
+                );
+            }
+
+
         long totalCount = directMessageRepository.countAllByConversationId(conversationId);
         //빈 리스트 체크
-        if (directMessages.isEmpty()) {
-            return new CursorResponseDirectMessageDto(
-                    new ArrayList<>(),
-                    null,
-                    null,
-                    false,
-                    totalCount,
-                    cond.getSortBy(),
-                    cond.getSortDirection()
-            );
-        }
+
         int originalSize = directMessages.size();
         boolean hasNext = originalSize > cond.getLimit();
         List<DirectMessage> result = hasNext
@@ -116,30 +165,25 @@ public class DirectMessageService {
     }
 
     public DirectMessageDto saveDirectMessage(UUID loginUserId,
+                                              UUID conversationId,
                                               DirectMessageSendRequest request
                                               ){
-        Conversation conversation = conversationRepository.findById(request.conversationId())
+        Conversation conversation = conversationRepository.findById(conversationId)
                 .orElseThrow(() -> ConversationNotFound.of());
 
         // 채팅방 참여자 일치 여부 검증
         UUID userA= conversation.getUser().getId();
         UUID userB = conversation.getWith().getId();
-        UUID receiverId = request.receiverId();
-
-        boolean matched =
-                (userA.equals(loginUserId) && userB.equals(receiverId)) ||
-                (userB.equals(loginUserId) && userA.equals(receiverId));
-        if (!matched) {
-            throw ConversationForbiddenException.withId(loginUserId);
-        }
 
         if (!userA.equals(loginUserId) && !userB.equals(loginUserId)) {
             throw ConversationForbiddenException.withId(loginUserId);
         }
 
+        UUID receiverId = userA.equals(loginUserId) ? userB : userA;
+
         User sender = userRepository.findById(loginUserId)
                 .orElseThrow(()-> new  UserNotFoundException(UserErrorCode.USER_NOT_FOUND, Map.of("userId", loginUserId)));
-        User receiver = userRepository.findById(request.receiverId())
+        User receiver = userRepository.findById(receiverId)
                 .orElseThrow(()-> new  UserNotFoundException(UserErrorCode.USER_NOT_FOUND, Map.of("userId", receiverId)));
         DirectMessage directMessage = directMessageRepository.save(DirectMessage.builder()
                 .sender(sender)

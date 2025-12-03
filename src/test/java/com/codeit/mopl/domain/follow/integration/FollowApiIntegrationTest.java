db34fb45d3c8f5860a0f768c1a8b040b8a9f8de9
LeeJihyeon <devlee1011@gmail.com>
2025-12-03 17:41:07 +0900

    fix(Follow, Infra): 테스트 환경 Redis 설정 삭제, ActiveProfiles 삭제
    테스트 환경용 Redis, Kafka는 추후 TestContainers를 사용하여 구현할 예정

--- src/test/java/com/codeit/mopl/domain/follow/integration/FollowApiIntegrationTest.java
+++ src/test/java/com/codeit/mopl/domain/follow/integration/FollowApiIntegrationTest.java
@@ -35,7 +35,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Transactional
-@ActiveProfiles("test")
 public class FollowApiIntegrationTest {
 
     @Autowired

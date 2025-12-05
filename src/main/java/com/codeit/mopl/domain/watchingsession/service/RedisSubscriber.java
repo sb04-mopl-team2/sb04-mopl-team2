073b008ed171de9784926b273c0f6fa79d33084e
Leejun <estherleejunkim@gmail.com>
2025-12-05 15:27:29 +0900

    feat(watchingsession): redissubscriber try catch 추가

--- src/main/java/com/codeit/mopl/domain/watchingsession/service/RedisSubscriber.java
+++ src/main/java/com/codeit/mopl/domain/watchingsession/service/RedisSubscriber.java
@@ -22,11 +22,14 @@ public class RedisSubscriber {
   public void onMessage(String message) throws JsonProcessingException {
     log.info("[웹소켓 (Redis)] onMessage - 받은 메시지 (raw): {}", message);
 
-    MessagePayload payload = mapper.readValue(message, MessagePayload.class);
-    String destination = payload.destination();
-
-    log.info("[웹소켓 (Redis)] onMessage - 메세지 수신, destination = {}", destination);
-    template.convertAndSend(payload.destination(), payload.content());
+    try {
+      MessagePayload payload = mapper.readValue(message, MessagePayload.class);
+      String destination = payload.destination();
+      log.info("[웹소켓 (Redis)] onMessage - 메세지 수신, destination = {}", destination);
+      template.convertAndSend(payload.destination(), payload.content());
+    } catch (JsonProcessingException e) {
+      log.error("[웹소켓 (Redis)] onMessage - 메시지 역직렬화 실패: {}", message, e);
+    }
   }
 
 }

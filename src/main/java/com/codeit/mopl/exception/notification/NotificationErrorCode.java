d7d06eb26721974d0046872006c8b551764dfb79
KwonYongJin <chaoskyj@gmail.com>
2025-12-09 14:11:53 +0900

    style(notification) : 불필요한 import 문 제거

--- src/main/java/com/codeit/mopl/exception/notification/NotificationErrorCode.java
+++ src/main/java/com/codeit/mopl/exception/notification/NotificationErrorCode.java
@@ -1,12 +1,11 @@
 package com.codeit.mopl.exception.notification;
 
 import com.codeit.mopl.exception.global.ErrorCodeInterface;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 public enum NotificationErrorCode implements ErrorCodeInterface {
   NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "알림을 찾을 수 없습니다."),
-  NOTIFICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "알림에 대한 권리가 없습니다.");
+  NOTIFICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "알림에 대한 권한이 없습니다.");
 
   private final HttpStatus status;
   private final String message;

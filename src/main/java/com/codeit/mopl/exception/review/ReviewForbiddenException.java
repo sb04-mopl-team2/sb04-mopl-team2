b5258c8a1920dd482dec7ab17fefb6bc99a4841f
KwonYongJin <chaoskyj@gmail.com>
2025-12-09 10:43:22 +0900

    style(review) : 스타일 통일 및 빈 공백 제거

--- src/main/java/com/codeit/mopl/exception/review/ReviewForbiddenException.java
+++ src/main/java/com/codeit/mopl/exception/review/ReviewForbiddenException.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
 public class ReviewForbiddenException extends ReviewException {
-
     public ReviewForbiddenException(ReviewErrorCode reviewErrorCode, Map<String, Object> details) {
         super(reviewErrorCode,details);
     }

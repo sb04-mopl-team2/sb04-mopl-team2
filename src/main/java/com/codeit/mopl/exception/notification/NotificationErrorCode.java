9f96714850368fe396a333432d8082c3017b996d
KwonYongJin <chaoskyj@gmail.com>
2025-12-09 10:43:55 +0900

    refactor(notification) : 다른 예외 클래스와 형식이 맞게 변경

--- src/main/java/com/codeit/mopl/exception/notification/NotificationErrorCode.java
+++ src/main/java/com/codeit/mopl/exception/notification/NotificationErrorCode.java
@@ -1,16 +1,33 @@
 package com.codeit.mopl.exception.notification;
 
+import com.codeit.mopl.exception.global.ErrorCodeInterface;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
-@Getter
-public enum NotificationErrorCode {
-  NOTIFICATION_NOT_FOUND("알림을 찾을 수 없습니다.",404),
-  NOTIFICATION_FORBIDDEN("알림을 수정할 권리가 없습니다.",403);
-  private String message;
-  private int code;
+public enum NotificationErrorCode implements ErrorCodeInterface {
+  NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "알림을 찾을 수 없습니다."),
+  NOTIFICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "알림에 대한 권리가 없습니다.");
 
-  NotificationErrorCode(String message, int code) {
+  private final HttpStatus status;
+  private final String message;
+
+  NotificationErrorCode(HttpStatus status, String message) {
+    this.status = status;
     this.message = message;
-    this.code = code;
+  }
+
+  @Override
+  public String getName() {
+    return this.name();
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+
+  @Override
+  public HttpStatus getStatus() {
+    return status;
   }
 }

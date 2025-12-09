e89ed778ae2a8d8bb0f05bc8dea2cc201af8f19e
Leejun <estherleejunkim@gmail.com>
2025-12-09 15:27:20 +0900

    clean(es): 주석 추가

--- src/main/java/com/codeit/mopl/search/converter/Converter.java
+++ src/main/java/com/codeit/mopl/search/converter/Converter.java
@@ -3,6 +3,7 @@ package com.codeit.mopl.search.converter;
 import com.codeit.mopl.search.document.AbstractDocument;
 import java.time.LocalDateTime;
 
+// E - Document 클래스, T - DTO
 public interface Converter<E extends AbstractDocument, T> {
 
   Class<E> getDocumentClass();

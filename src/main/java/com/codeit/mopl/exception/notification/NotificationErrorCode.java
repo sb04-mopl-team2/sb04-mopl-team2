c31ea9a9a5e0eee678436a8c9153b96a606ac3d6
KwonYongJin <chaoskyj@gmail.com>
2025-12-09 14:18:27 +0900

    refactor(notification) : 예외 클래스에 Getter 사용

--- src/main/java/com/codeit/mopl/exception/notification/NotificationErrorCode.java
+++ src/main/java/com/codeit/mopl/exception/notification/NotificationErrorCode.java
@@ -1,8 +1,12 @@
 package com.codeit.mopl.exception.notification;
 
 import com.codeit.mopl.exception.global.ErrorCodeInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@AllArgsConstructor
 public enum NotificationErrorCode implements ErrorCodeInterface {
   NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "알림을 찾을 수 없습니다."),
   NOTIFICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "알림에 대한 권한이 없습니다.");
@@ -10,23 +14,8 @@ public enum NotificationErrorCode implements ErrorCodeInterface {
   private final HttpStatus status;
   private final String message;
 
-  NotificationErrorCode(HttpStatus status, String message) {
-    this.status = status;
-    this.message = message;
-  }
-
   @Override
   public String getName() {
     return this.name();
-  }
-
-  @Override
-  public String getMessage() {
-    return message;
-  }
-
-  @Override
-  public HttpStatus getStatus() {
-    return status;
   }
 }

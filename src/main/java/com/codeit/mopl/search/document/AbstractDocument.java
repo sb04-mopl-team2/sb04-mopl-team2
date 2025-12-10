6f704a7933d15efff3a6f841848909f7f2288300
Leejun <estherleejunkim@gmail.com>
2025-12-10 15:12:23 +0900

    fix(opensearch): abstractdocument 수정

--- src/main/java/com/codeit/mopl/search/document/AbstractDocument.java
+++ src/main/java/com/codeit/mopl/search/document/AbstractDocument.java
@@ -5,17 +5,12 @@ import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AbstractDocument {
   @Id
-  @Field(type = FieldType.Keyword)
   private String id;
-  @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
   private LocalDateTime createdAt;
 }

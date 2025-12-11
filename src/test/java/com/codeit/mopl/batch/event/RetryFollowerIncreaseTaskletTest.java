c841828dd6b12335c46af5ac80f8195dbb8e6dfb
JihyeonLee <devlee1011@gmail.com>
2025-12-11 11:58:51 +0900

    feat(Follow): PENDING 상태 팔로우 객체 재시도 배치 처리 테스트 성공, 중단 테스트 코드 작성

--- src/test/java/com/codeit/mopl/batch/event/RetryFollowerIncreaseTaskletTest.java
+++ src/test/java/com/codeit/mopl/batch/event/RetryFollowerIncreaseTaskletTest.java
@@ -6,64 +6,61 @@ import com.codeit.mopl.domain.follow.entity.FollowStatus;
 import com.codeit.mopl.domain.follow.repository.FollowRepository;
 import com.codeit.mopl.domain.follow.service.FollowService;
 import com.codeit.mopl.domain.user.entity.User;
-import com.codeit.mopl.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class RetryFollowerIncreaseTaskletTest {
 
-    @Autowired
-    private PendingEventRetryStepConfig stepConfig;
-
-    @MockitoBean
+    @Mock
     private FollowService followService;
 
-    @MockitoBean
+    @Mock
     private FollowRepository followRepository;
 
-    @MockitoBean
-    private UserRepository userRepository;
+    @InjectMocks
+    private PendingEventRetryStepConfig stepConfig;
 
     @Test
-    @DisplayName("Pending 상태의 follow 객체의 팔로워 수 증가 성공")
+    @DisplayName("Pending 상태 follow 객체 팔로워 수 증가 성공")
     public void retryFollowerIncreaseTasklet_Success() throws Exception {
         // given
         User follower = new User();
         UUID followerId = UUID.randomUUID();
         ReflectionTestUtils.setField(follower, "id", followerId);
-        userRepository.saveAndFlush(follower);
 
         User followee = new User();
         UUID followeeId = UUID.randomUUID();
         ReflectionTestUtils.setField(followee, "id", followeeId);
         followee.setFollowerCount(0L);
-        userRepository.saveAndFlush(followee);
 
         Follow follow = new Follow(follower, followee);
         UUID followId = UUID.randomUUID();
         ReflectionTestUtils.setField(follow, "id", followId);
         follow.setFollowStatus(FollowStatus.PENDING);
-        followRepository.saveAndFlush(follow);
+        follow.setRetryCount(0);
 
         Tasklet tasklet = stepConfig.retryFollowerIncreaseTasklet();
+
+
+        given(followRepository.findByStatus(eq(FollowStatus.PENDING)))
+                .willReturn(List.of(follow));
 
         // when
         RepeatStatus status = tasklet.execute(null, null);
@@ -76,6 +73,21 @@ public class RetryFollowerIncreaseTaskletTest {
         verify(followService, times(1))
                 .processFollowerIncrease(eq(followId), eq(followeeId));
         verify(followRepository, times(1)).save(follow);
+    }
 
+    @Test
+    @DisplayName("Pending 상태 follow 객체 팔로워 수 증가 중단 - PENDING 상태의 팔로우 객체가 없습니다.")
+    void retryFollowerIncreaseTasklet_NoPendingFollows() throws Exception {
+        // given
+        Tasklet tasklet = stepConfig.retryFollowerIncreaseTasklet();
+
+        given(followRepository.findByStatus(eq(FollowStatus.PENDING)))
+                .willReturn(List.of());
+
+        // when
+        RepeatStatus status = tasklet.execute(null, null);
+
+        // then
+        assertEquals(RepeatStatus.FINISHED, status);
     }
 }

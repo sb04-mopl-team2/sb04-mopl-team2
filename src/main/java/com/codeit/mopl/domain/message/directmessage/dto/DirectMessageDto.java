475423b2dbe547068741fb51349f9b6e8ef4bb83
문은서 <meunse1999@naver.com>
2025-12-12 16:22:43 +0900

    feat(message): dm엔티티에 팩토리 메서드 추가
    - 시간 표현 변환을 위한 팩토리 메서드 from()추가

--- src/main/java/com/codeit/mopl/domain/message/directmessage/dto/DirectMessageDto.java
+++ src/main/java/com/codeit/mopl/domain/message/directmessage/dto/DirectMessageDto.java
@@ -1,16 +1,39 @@
 package com.codeit.mopl.domain.message.directmessage.dto;
 
+import com.codeit.mopl.domain.message.directmessage.entity.DirectMessage;
 import com.codeit.mopl.domain.user.dto.response.UserSummary;
 
 import java.time.Instant;
+import java.time.ZoneId;
 import java.util.UUID;
 
 public record DirectMessageDto(
     UUID id,
     UUID conversationId,
-    Instant createdAt,
+    String createdAt,
     UserSummary sender,
     UserSummary receiver,
     String content
 ) {
+    public static DirectMessageDto from(DirectMessage dm) {
+        return new DirectMessageDto(
+                dm.getId(),
+                dm.getConversation().getId(),
+                dm.getCreatedAt()
+                        .atZone(ZoneId.of("Asua/Seoul"))
+                        .toLocalDateTime()
+                        .toString(),
+                new UserSummary(
+                        dm.getSender().getId(),
+                        dm.getSender().getName(),
+                        dm.getSender().getProfileImageUrl()
+                ),
+                new UserSummary(
+                        dm.getReceiver().getId(),
+                        dm.getReceiver().getName(),
+                        dm.getReceiver().getProfileImageUrl()
+                ),
+                dm.getContent()
+        );
+    }
 }

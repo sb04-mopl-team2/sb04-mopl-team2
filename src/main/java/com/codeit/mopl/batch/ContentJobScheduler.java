7d3a51a35314711f59bfe13135ab86d7c58cab49
Oh-Myeongjae <omjcws@gmail.com>
2025-12-12 14:26:03 +0900

    fix(batch): 401에러 수정을 위해 변경했던 스케쥴러 시간 변경 및 로그추가 수정

--- src/main/java/com/codeit/mopl/batch/ContentJobScheduler.java
+++ src/main/java/com/codeit/mopl/batch/ContentJobScheduler.java
@@ -22,8 +22,7 @@ public class ContentJobScheduler {
   /**
    * 매일 새벽 4시에 Movie, TV, Sports를 순차적으로 실행
    */
-//  @Scheduled(cron = "0 30 8 * * *")
-  @Scheduled(cron = "0 */5 * * * *")
+  @Scheduled(cron = "0 30 8 * * *", zone = "Asia/Seoul")
   public void runDailyContentUpdate() {
     log.info("=== 일일 컨텐츠 업데이트 시작 (Movie + TV + Sports) ===");
 

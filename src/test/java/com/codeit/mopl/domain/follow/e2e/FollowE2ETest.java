97ddee8e6f60d58b67286561a87f4e462aa9553f
JihyeonLee <devlee1011@gmail.com>
2025-12-12 17:35:59 +0900

    test(Follow): 팔로워 여부 조회, 팔로워 수 조회 E2E 테스트 추가

--- src/test/java/com/codeit/mopl/domain/follow/e2e/FollowE2ETest.java
+++ src/test/java/com/codeit/mopl/domain/follow/e2e/FollowE2ETest.java
@@ -10,6 +10,7 @@ import com.codeit.mopl.domain.follow.repository.FollowRepository;
 import com.codeit.mopl.domain.notification.repository.NotificationRepository;
 import com.codeit.mopl.domain.user.dto.request.UserCreateRequest;
 import com.codeit.mopl.domain.user.dto.response.UserDto;
+import com.codeit.mopl.domain.user.entity.User;
 import com.codeit.mopl.domain.user.repository.UserRepository;
 import com.codeit.mopl.exception.global.ErrorResponse;
 import org.junit.jupiter.api.*;
@@ -24,6 +25,7 @@ import org.springframework.util.MultiValueMap;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -82,6 +84,7 @@ public class FollowE2ETest {
         HttpEntity followerLoginEntity = getSignInRequest(followerSignInRequest);
         ResponseEntity<JwtDto> loginJwtDto = restTemplate.postForEntity("/api/auth/sign-in", followerLoginEntity, JwtDto.class);
         followerAccessToken = loginJwtDto.getBody().accessToken();
+        defaultHeaders.setBearerAuth(followerAccessToken);
     }
 
     @AfterEach
@@ -96,16 +99,7 @@ public class FollowE2ETest {
     void createFollow_Success() {
         // given
         FollowRequest followRequest = new FollowRequest(followee.id());
-
-        // 헤더 생성: AccessToken, CSRF
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(followerAccessToken);
-
-        headers.add(HttpHeaders.COOKIE, defaultHeaders.getFirst(HttpHeaders.COOKIE));
-        headers.add("X-XSRF-TOKEN", defaultHeaders.getFirst("X-XSRF-TOKEN"));
-
-        HttpEntity<FollowRequest> entity = new HttpEntity<>(followRequest, headers);
+        HttpEntity<FollowRequest> entity = new HttpEntity<>(followRequest, defaultHeaders);
 
         // when
         ResponseEntity<FollowDto> response = restTemplate.postForEntity("/api/follows", entity, FollowDto.class);
@@ -130,16 +124,7 @@ public class FollowE2ETest {
     void createFollow_Failure_FollowSelfProhibitedException() {
         // given
         FollowRequest followRequest = new FollowRequest(follower.id());
-
-        // 헤더 생성: AccessToken, CSRF
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(followerAccessToken);
-
-        headers.add(HttpHeaders.COOKIE, defaultHeaders.getFirst(HttpHeaders.COOKIE));
-        headers.add("X-XSRF-TOKEN", defaultHeaders.getFirst("X-XSRF-TOKEN"));
-
-        HttpEntity<FollowRequest> entity = new HttpEntity<>(followRequest, headers);
+        HttpEntity<FollowRequest> entity = new HttpEntity<>(followRequest, defaultHeaders);
 
         // when
         ResponseEntity<ErrorResponse> response =
@@ -161,16 +146,7 @@ public class FollowE2ETest {
     void createFollow_Failure_FollowDuplicateException() {
         // given
         FollowRequest followRequest = new FollowRequest(followee.id());
-
-        // 헤더 생성: AccessToken, CSRF
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(followerAccessToken);
-
-        headers.add(HttpHeaders.COOKIE, defaultHeaders.getFirst(HttpHeaders.COOKIE));
-        headers.add("X-XSRF-TOKEN", defaultHeaders.getFirst("X-XSRF-TOKEN"));
-
-        HttpEntity<FollowRequest> entity = new HttpEntity<>(followRequest, headers);
+        HttpEntity<FollowRequest> entity = new HttpEntity<>(followRequest, defaultHeaders);
 
         // when
         // 첫 번째 팔로우 생성
@@ -188,6 +164,125 @@ public class FollowE2ETest {
         assertThat(errorResponse.message()).contains("같은 사용자를 중복해서 팔로우할 수 없습니다.");
         assertThat(errorResponse.details().get("followerId")).isEqualTo(follower.id().toString());
         assertThat(errorResponse.details().get("followeeId")).isEqualTo(followee.id().toString());
+    }
+
+    @Test
+    @DisplayName("팔로우 여부 조회 성공 - 팔로우 된 상태면 true 반환")
+    void isFollowedByMe_Success_isFollowedTrue() {
+        // given
+        // 팔로우 생성
+        FollowRequest followRequest = new FollowRequest(followee.id());
+        HttpEntity<FollowRequest> followHttpEntity = new HttpEntity<>(followRequest, defaultHeaders);
+        restTemplate.postForEntity("/api/follows", followHttpEntity, FollowDto.class);
+
+        // 팔로우 여부 조회용 HttpEntity 생성
+        HttpEntity<Void> entity = new HttpEntity<>(defaultHeaders);
+
+        // when
+        ResponseEntity<Boolean> response = restTemplate.exchange(
+                "/api/follows/followed-by-me?followeeId=" + followee.id(),
+                HttpMethod.GET,
+                entity,
+                Boolean.class
+        );
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isTrue();
+    }
+
+    @Test
+    @DisplayName("팔로우 여부 조회 성공 - 팔로우 안된 상태면 false 반환")
+    void isFollowedByMe_Success_isFollowedFalse() {
+        // given
+        HttpEntity<Void> entity = new HttpEntity<>(defaultHeaders);
+
+        // when
+        ResponseEntity<Boolean> response = restTemplate.exchange(
+                "/api/follows/followed-by-me?followeeId=" + followee.id(),
+                HttpMethod.GET,
+                entity,
+                Boolean.class
+        );
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isFalse();
+    }
+
+    @Test
+    @DisplayName("팔로우 여부 조회 실패 - 존재하지 않는 유저")
+    void isFollowedByMe_Failure_UserNotFoundException() {
+        // given
+        UUID followeeId = UUID.randomUUID();
+        HttpEntity<Void> entity = new HttpEntity<>(defaultHeaders);
+
+        // when
+        ResponseEntity<ErrorResponse> response = restTemplate.exchange(
+                "/api/follows/followed-by-me?followeeId=" + followeeId,
+                HttpMethod.GET,
+                entity,
+                ErrorResponse.class
+        );
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+
+        ErrorResponse errorResponse = response.getBody();
+        assertThat(errorResponse).isNotNull();
+        assertThat(errorResponse.exceptionName()).isEqualTo("USER_NOT_FOUND");
+        assertThat(errorResponse.message()).contains("유저를 찾을 수 없습니다.");
+        assertThat(errorResponse.details().get("userId")).isEqualTo(followeeId.toString());
+    }
+
+    @Test
+    @DisplayName("팔로워 수 조회 성공")
+    void getFollowerCount_Success() {
+        // given
+        HttpEntity<Void> entity = new HttpEntity<>(defaultHeaders);
+
+        // when
+        ResponseEntity<Long> response = restTemplate.exchange(
+                "/api/follows/count?followeeId=" + followee.id(),
+                HttpMethod.GET,
+                entity,
+                Long.class
+        );
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        Long followerCount = response.getBody();
+        assertThat(followerCount).isEqualTo(0L);
+        
+        // 실제 값과 비교
+        User followeeUser = userRepository.findById(followee.id()).orElse(null);
+        assertThat(followeeUser).isNotNull();
+        assertThat(followeeUser.getFollowerCount()).isEqualTo(followerCount);
+    }
+
+    @Test
+    @DisplayName("팔로워 수 조회 실패 - 존재하지 않는 유저")
+    void getFollowerCount_Failure_UserNotFoundException() {
+        // given
+        HttpEntity<Void> entity = new HttpEntity<>(defaultHeaders);
+        UUID followeeId = UUID.randomUUID();
+
+        // when
+        ResponseEntity<ErrorResponse> response = restTemplate.exchange(
+                "/api/follows/count?followeeId=" + followeeId,
+                HttpMethod.GET,
+                entity,
+                ErrorResponse.class
+        );
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        ErrorResponse errorResponse = response.getBody();
+        assertThat(errorResponse).isNotNull();
+        assertThat(errorResponse.exceptionName()).isEqualTo("USER_NOT_FOUND");
+        assertThat(errorResponse.message()).contains("유저를 찾을 수 없습니다.");
+        assertThat(errorResponse.details().get("userId")).isEqualTo(followeeId.toString());
     }
 
     private void setCSRFToken() {

17bbd9506dab918c6693213d742fa32c422c5aeb
문은서 <meunse1999@naver.com>
2026-01-15 22:17:31 +0900

    feat: 캐싱을 위한 dto추가

--- src/main/java/com/codeit/mopl/domain/playlist/dto/PlaylistCachedDto.java
+++ src/main/java/com/codeit/mopl/domain/playlist/dto/PlaylistCachedDto.java
@@ -7,7 +7,7 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-public record PaylistCachedDto(
+public record PlaylistCachedDto(
         UUID id,
         UserSummary owner,
         String title,

263b5227f189cbc0b458be20f768a770f07e9557
JihyeonLee <devlee1011@gmail.com>
2026-01-16 22:33:05 +0900

    refactor(OutBox): ErrorMessageSummarizer 유틸 클래스 로직 간소화

--- src/main/java/com/codeit/mopl/outbox/util/ErrorMessageSummarizer.java
+++ src/main/java/com/codeit/mopl/outbox/util/ErrorMessageSummarizer.java
@@ -1,17 +1,19 @@
 package com.codeit.mopl.outbox.util;
 
-public class ErrorMessageSummarizer {
+public final class ErrorMessageSummarizer {
+
+    public ErrorMessageSummarizer() {
+    }
+
+    private static final int MAX_LENGTH = 4000;
+    private static final String ELLIPSIS = "...";
 
     public static String summarizeErrorMessage(String errorMessage) {
         if (errorMessage == null) {
             return null;
         }
-
-        if (errorMessage.length() > 4000) {
-            errorMessage = errorMessage.substring(0, 3997) + "...";
-            return errorMessage;
-        } else {
-            return errorMessage;
-        }
+        return errorMessage.length() > MAX_LENGTH
+                ? errorMessage.substring(0, MAX_LENGTH - ELLIPSIS.length()) + ELLIPSIS
+                : errorMessage;
     }
 }

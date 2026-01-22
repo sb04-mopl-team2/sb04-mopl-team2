d0cddcb874a0bd98c9ef7db10af26a117495aff3
JihyeonLee <devlee1011@gmail.com>
2026-01-22 22:51:54 +0900

    refactor(OutBox): EventSerializer 예외 처리에 null 분기 추가
    - EventSerializationFailedException
    - EventDeserializationFailedException

--- src/main/java/com/codeit/mopl/exception/outbox/EventDeserializationFailedException.java
+++ src/main/java/com/codeit/mopl/exception/outbox/EventDeserializationFailedException.java
@@ -1,5 +1,6 @@
 package com.codeit.mopl.exception.outbox;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class EventDeserializationFailedException extends OutBoxException {
@@ -8,7 +9,12 @@ public class EventDeserializationFailedException extends OutBoxException {
     }
 
     public static EventDeserializationFailedException withDetails(Object event) {
-        Map<String, Object> details = Map.of("eventClass", event.getClass().getSimpleName());
+        Map<String, Object> details = new HashMap<>();
+        if (event == null) {
+            details.put("eventClass", "null");
+            return new EventDeserializationFailedException(details);
+        }
+        details.put("eventClass", event.getClass().getSimpleName());
         return new EventDeserializationFailedException(details);
     }
 }
--- src/main/java/com/codeit/mopl/exception/outbox/EventSerializationFailedException.java
+++ src/main/java/com/codeit/mopl/exception/outbox/EventSerializationFailedException.java
@@ -1,5 +1,6 @@
 package com.codeit.mopl.exception.outbox;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class EventSerializationFailedException extends OutBoxException {
@@ -8,7 +9,12 @@ public class EventSerializationFailedException extends OutBoxException {
     }
 
     public static EventSerializationFailedException withDetails(Object event) {
-        Map<String, Object> details = Map.of("eventClass", event.getClass().getSimpleName());
+        Map<String, Object> details = new HashMap<>();
+        if (event == null) {
+            details.put("eventClass", "null");
+            return new EventSerializationFailedException(details);
+        }
+        details.put("eventClass", event.getClass().getSimpleName());
         return new EventSerializationFailedException(details);
     }
 }

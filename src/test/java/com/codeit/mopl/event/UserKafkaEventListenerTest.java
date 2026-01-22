4ef8846796218fb4ac7b1456942ac2799c9b0881
JihyeonLee <devlee1011@gmail.com>
2026-01-22 21:47:17 +0900

    refactor(Kafka): UserKafkaEventListener 수정

--- src/test/java/com/codeit/mopl/event/UserKafkaEventListenerTest.java
+++ src/test/java/com/codeit/mopl/event/UserKafkaEventListenerTest.java
@@ -4,8 +4,7 @@ import com.codeit.mopl.domain.user.entity.Role;
 import com.codeit.mopl.event.event.UserLogInOutEvent;
 import com.codeit.mopl.event.event.UserRoleUpdateEvent;
 import com.codeit.mopl.event.listener.KafkaEventListener;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import com.codeit.mopl.event.sender.KafkaEventSender;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,21 +13,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.MDC;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserKafkaEventListenerTest {
+
     @Mock
-    private KafkaTemplate<String, String> kafkaTemplate;
-    @Mock
-    private ObjectMapper objectMapper;
+    private KafkaEventSender sender;
 
     @InjectMocks
     private KafkaEventListener kafkaEventListener;
@@ -36,10 +30,8 @@ public class UserKafkaEventListenerTest {
     @BeforeEach
     void setUp() {
         MDC.put("requestId", UUID.randomUUID().toString());
-        CompletableFuture<SendResult<String, String>> future =
-                (CompletableFuture<SendResult<String, String>>) mock(CompletableFuture.class);
-        when(kafkaTemplate.send(any(ProducerRecord.class))).thenReturn(future);
     }
+
     @Test
     @DisplayName("UserRoleUpdateEventListener")
     void userRoleUpdateEventListener() {
@@ -56,7 +48,11 @@ public class UserKafkaEventListenerTest {
         kafkaEventListener.on(event);
 
         // then
-        verify(kafkaTemplate, times(1)).send(any(ProducerRecord.class));
+        verify(sender, times(1)).send(
+                eq("mopl-user-role-update"),
+                eq(userId.toString()),
+                eq(event)
+        );
     }
 
     @Test
@@ -72,6 +68,10 @@ public class UserKafkaEventListenerTest {
         kafkaEventListener.on(event);
 
         // then
-        verify(kafkaTemplate, times(1)).send(any(ProducerRecord.class));
+        verify(sender, times(1)).send(
+                eq("mopl-user-login-out"),
+                eq(userId.toString()),
+                eq(event)
+        );
     }
 }
